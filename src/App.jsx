a669d9902b4c51f6e069ea245ef4284fceb9a686
M <dharanidharan.m@capgemini.com>
2023-12-14 16:32:45 +0530

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,7 @@ const App = () => {
     <>
       <Header />
       <Nav />
-      {/* changed line */}
-      {/* <About /> */}
+      <About />
       <Experience />
       <Services />
       <Portfolio />
